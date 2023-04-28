0dc08bde07fa97536ce2c0a165bf72a972efc7f6
sivakrishnareddy.p <sivakrishnareddy.p@presidio.com>
2023-04-28 17:20:48 +0530

    Flutter - Added model and Dummy Data

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_expense_tracker/expenses_list.dart';
+import 'package:flutter_expense_tracker/models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -10,11 +12,27 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registedExpenses = [
+    Expense(
+        title: "flutter",
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: "food",
+        amount: 12.99,
+        date: DateTime.now(),
+        category: Category.food)
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Expense Tracker")),
-      body: Column(children: [Text("The Chart"), Text("Expenses List")]),
+      body: Column(children: [
+        const Text("The Chart"),
+        Expanded(child: ExpensesList(expenses: _registedExpenses))
+      ]),
     );
   }
 }
